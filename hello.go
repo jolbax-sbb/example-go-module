92d568d28e2486b5c8b5ab05ca1b454e06a37c19
jolbax-sbb <jose_luis.barahona@sbb.ch>
2023-05-19 14:43:03 +0200

    feat: add external quote module

--- hello.go
+++ hello.go
@@ -3,7 +3,7 @@ package example_go_module
 import "fmt"
 import "rsc.io/quote"
 
-// Hello is a function that prints a "hello world"
+// Hello is a function that prints a some useful text
 func Hello() {
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Go())
